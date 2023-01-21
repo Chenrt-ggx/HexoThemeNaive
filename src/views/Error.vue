c2fa18a282f8aaf88d873e885ab13b3f4470a276
Chenrt <2926777985@qq.com>
2023-01-21 15:54:42 +0800

    add error page

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,3 +1,39 @@
 <template>
-  <div>{{ $route.meta.title }}</div>
+  <div class="page-container">
+    <div class="text-center">
+      <img src="@/assets/404.svg" alt="404" />
+    </div>
+    <div class="text-center">
+      <h1>抱歉，你访问的页面不存在</h1>
+      <router-link to="/" style="text-decoration: none">
+        <n-button type="info" @click="goHome">回到首页</n-button>
+      </router-link>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { NButton } from 'naive-ui';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goHome = () => router.push('/');
+</script>
+
+<style scoped lang="scss">
+.page-container {
+  padding: 14vh 0;
+
+  .text-center {
+    h1 {
+      color: #666;
+      padding: 20px 0;
+    }
+  }
+
+  img {
+    width: 80%;
+    max-width: 400px;
+  }
+}
+</style>
